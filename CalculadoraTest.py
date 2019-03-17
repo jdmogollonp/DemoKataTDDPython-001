67c4ca19dfbc8da227e6698735951237b10177a6
Juan Mogollon <jd.mogollon@uniandes.edu.co>
2019-03-17 11:42:52 -0500

    Paso 2- Se corrige codigo para que funcione con numeros distintos (Verde)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -8,6 +8,7 @@ class CalculadoraTest(TestCase):
     def test_sumar_unacadena(self):
           self.assertEqual(Calculadora().sumar("1"),1,"Un numero")
 
+
     def test_sumar_cadenaConUnNumero(self):
          self.assertEqual(Calculadora().sumar("1"),1,"Un número")
          self.assertEqual(Calculadora().sumar("2"), 2, "Un número")
